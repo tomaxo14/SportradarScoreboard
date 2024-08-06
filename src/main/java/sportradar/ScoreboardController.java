654eb5dcb1dc54ed481834f108fc6e58441750b0
tomaxo14 <tomek.przybylski@onet.pl>
2024-08-06 22:51:09 +0200

    Business logic moved to additional service layer

--- src/main/java/sportradar/ScoreboardController.java
+++ src/main/java/sportradar/ScoreboardController.java
@@ -1,87 +1,30 @@
 package sportradar;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
+import lombok.RequiredArgsConstructor;
 import sportradar.exception.IdNotFoundException;
 import sportradar.exception.NegativeScoreException;
 import sportradar.model.Match;
-import sportradar.model.Team;
 
+@RequiredArgsConstructor
 public class ScoreboardController {
 
-    private int idIterator = 0;
-    private List<Match> ongoingMatches = new ArrayList<>();
+    private final ScoreboardService service;
 
     public Match startMatch(String homeTeamName, String awayTeamName) {
-
-        idIterator += 1;
-        Match newMatch = Match.builder()
-                .id(idIterator)
-                .homeTeam(new Team(homeTeamName))
-                .awayTeam(new Team(awayTeamName))
-                .homeTeamGoals(0)
-                .awayTeamGoals(0)
-                .startDateTime(LocalDateTime.now().plusSeconds(idIterator))
-                .build();
-        ongoingMatches.add(newMatch);
-
-        return newMatch;
+        return service.startMatch(homeTeamName, awayTeamName);
     }
 
     public Match updateScore(int matchId, int homeTeamScore, int awayTeamScore) throws NegativeScoreException, IdNotFoundException {
-
-        if (homeTeamScore < 0 || awayTeamScore < 0) {
-            throw new NegativeScoreException("You cannot pass negative numbers as a score");
-        }
-
-        Optional<Match> optionalMatchToUpdate = ongoingMatches.stream().filter(match -> match.id() == matchId).findFirst();
-        Match matchToUpdate;
-        if (optionalMatchToUpdate.isPresent()) {
-            matchToUpdate = optionalMatchToUpdate.get();
-            ongoingMatches.remove(matchToUpdate);
-        } else {
-            throw new IdNotFoundException("Match with given ID not found");
-        }
-        matchToUpdate.homeTeamGoals(homeTeamScore);
-        matchToUpdate.awayTeamGoals(awayTeamScore);
-        ongoingMatches.add(matchToUpdate);
-
-        return matchToUpdate;
+        return service.updateScore(matchId, homeTeamScore, awayTeamScore);
     }
 
     public List<Match> finishMatch(int matchId) throws IdNotFoundException {
-        Optional<Match> optionalMatchToDelete = ongoingMatches.stream().filter(match -> match.id() == matchId).findFirst();
-
-        if (optionalMatchToDelete.isPresent()) {
-            ongoingMatches.remove(optionalMatchToDelete.get());
-        } else {
-            throw new IdNotFoundException("Match with given ID not found");
-        }
-
-        if (ongoingMatches.isEmpty()) {
-            idIterator = 0;
-        }
-
-        return ongoingMatches;
+        return service.finishMatch(matchId);
     }
 
     public List<Match> showOngoingMatches() {
-
-        Comparator<Match> startDateTimeComparator = Comparator
-                .comparing(Match::getStartDateTime)
-                .reversed();
-
-        Comparator<Match> goalsComparator = Comparator
-                .comparingInt(Match::getSumOfGoals)
-                .reversed();
-
-        ongoingMatches.sort(startDateTimeComparator);
-        ongoingMatches.sort(goalsComparator);
-
-        return ongoingMatches;
+        return service.showOngoingMatches();
     }
 }
--- src/main/java/sportradar/ScoreboardService.java
+++ src/main/java/sportradar/ScoreboardService.java
@@ -1,29 +1,87 @@
 package sportradar;
 
-
+import sportradar.exception.IdNotFoundException;
+import sportradar.exception.NegativeScoreException;
 import sportradar.model.Match;
+import sportradar.model.Team;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class ScoreboardService {
 
+    private int idIterator = 0;
+    private final List<Match> ongoingMatches = new ArrayList<>();
+
     public Match startMatch(String homeTeamName, String awayTeamName) {
 
-        return null;
+        idIterator += 1;
+        Match newMatch = Match.builder()
+                .id(idIterator)
+                .homeTeam(new Team(homeTeamName))
+                .awayTeam(new Team(awayTeamName))
+                .homeTeamGoals(0)
+                .awayTeamGoals(0)
+                .startDateTime(LocalDateTime.now().plusSeconds(idIterator))
+                .build();
+        ongoingMatches.add(newMatch);
+
+        return newMatch;
     }
 
-    public Match updateScore(int matchId, int homeTeamScore, int awayTeamScore) {
+    public Match updateScore(int matchId, int homeTeamScore, int awayTeamScore) throws NegativeScoreException, IdNotFoundException {
 
-        return null;
+        if (homeTeamScore < 0 || awayTeamScore < 0) {
+            throw new NegativeScoreException("You cannot pass negative numbers as a score");
+        }
+
+        Optional<Match> optionalMatchToUpdate = ongoingMatches.stream().filter(match -> match.id() == matchId).findFirst();
+        Match matchToUpdate;
+        if (optionalMatchToUpdate.isPresent()) {
+            matchToUpdate = optionalMatchToUpdate.get();
+            ongoingMatches.remove(matchToUpdate);
+        } else {
+            throw new IdNotFoundException("Match with given ID not found");
+        }
+        matchToUpdate.homeTeamGoals(homeTeamScore);
+        matchToUpdate.awayTeamGoals(awayTeamScore);
+        ongoingMatches.add(matchToUpdate);
+
+        return matchToUpdate;
     }
 
-    public List<Match> finishMatch(int matchId) {
+    public List<Match> finishMatch(int matchId) throws IdNotFoundException {
+        Optional<Match> optionalMatchToDelete = ongoingMatches.stream().filter(match -> match.id() == matchId).findFirst();
 
-        return null;
+        if (optionalMatchToDelete.isPresent()) {
+            ongoingMatches.remove(optionalMatchToDelete.get());
+        } else {
+            throw new IdNotFoundException("Match with given ID not found");
+        }
+
+        if (ongoingMatches.isEmpty()) {
+            idIterator = 0;
+        }
+
+        return ongoingMatches;
     }
 
     public List<Match> showOngoingMatches() {
 
-        return null;
+        Comparator<Match> startDateTimeComparator = Comparator
+                .comparing(Match::getStartDateTime)
+                .reversed();
+
+        Comparator<Match> goalsComparator = Comparator
+                .comparingInt(Match::getSumOfGoals)
+                .reversed();
+
+        ongoingMatches.sort(startDateTimeComparator);
+        ongoingMatches.sort(goalsComparator);
+
+        return ongoingMatches;
     }
 }
